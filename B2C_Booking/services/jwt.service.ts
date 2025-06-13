5bc63a5afec3d1d3782b2578cf1b90a08c344f1f
Rkrock30 <86731759+Rkrock30@users.noreply.github.com>
2025-06-13 18:59:23 +0530

    Delete B2C_Booking directory

--- B2C_Booking/services/jwt.service.ts
+++
@@ -1,6 +0,0 @@
-// services/jwt.service.ts
-import jwt from 'jsonwebtoken';
-
-export const generateToken = (payload: object, expiresIn: string) => {
-  return jwt.sign(payload, process.env.JWT_SECRET!, { expiresIn });
-};
--- B2C_Booking/types/booking.d.ts
+++
@@ -1,7 +0,0 @@
-export interface Booking {
-    destination: string;
-    from: string;
-    to: string;
-    last_travel_date: string;
-    highlights: string[];
-  }
